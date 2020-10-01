425b2eeb9c9f0bb9307f90f2051738ffcad9000e
glauco_basilio <glauco.basilio@rimisan.com.br>
2020-10-01 14:30:07 -0300

    Corrigido e adicionado exemplo de video.

---
+++ close-up-call/video/javascript/variables.js
@@ -0,0 +1,10 @@
+/* Javascript Document */
+
+//Mercury Content API Configurations
+var mercury_audio_css={};
+var mercury_popup_css={};
+var mercury_tooltip_css={};
+var mercury_video_css={};
+var mercury_zoom_css={};
+
+//Extra Configurations
